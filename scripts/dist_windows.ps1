3a3675389c0553b68e98a50d67b9f92a9fd58dc6
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2023-04-17 16:50:46 +0100

    Add WIX path

--- scripts/dist_windows.ps1
+++ scripts/dist_windows.ps1
@@ -1,2 +1,6 @@
 cargo install cargo-wix
 cargo wix --package glyphs-exchange
+
+# Need to do this, or powershell just pretends all is fine when the last process
+# complained?! See https://github.com/PowerShell/PowerShell/issues/11712.
+exit $LASTEXITCODE
