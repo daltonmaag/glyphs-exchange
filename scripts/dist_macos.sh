b2b4379388fb8adff2891527df4ff067de57b9ca
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2023-04-19 10:27:18 +0100

    Ship Universal binaries for macOS

--- scripts/dist_macos.sh
+++ scripts/dist_macos.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+cargo build --locked --release --target aarch64-apple-darwin
+cargo build --locked --release --target x86_64-apple-darwin
+
+mkdir -p target/universal
+lipo -create -output target/universal/glyphs-exchange \
+    target/aarch64-apple-darwin/release/glyphs-exchange \
+    target/x86_64-apple-darwin/release/glyphs-exchange
+
 /usr/local/bin/packagesbuild glyphs-exchange.pkgproj
